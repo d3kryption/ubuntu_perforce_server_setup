27dd8f99cf932f995edf1e2abb997020678d4abe
D3kryption <48034534+d3kryption@users.noreply.github.com>
2023-11-28 17:17:58 +0000

    Init upload
    # Added
    - Readme.MD with instructions
    - Actual perforce setup script

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
-# ubuntu_perforce_server_setup
+# Ubuntu Perforce server setup script
+
 This bash script will setup Perforce a good 90% of the way. Running all the commands you need to run it. Making it nice and easy to get setup and working.
+
+## Why?
+
+I had a few clients who wanted it setup and it was the same commands over and over again, so a quick bash script and now its easier than ever.
+
+## Limitations
+- Ubuntu based only - I tried to get it working on Debian what I normally use but either packages weren't available or they were out of date. Without a lot of messing around updating sources its just easier to use Ubuntu for now.
+
+## 🛠️ Installation Steps - development
+
+1) Clone the repo / download the `perforce_server_setup.sh`
+
+```bash
+git clone https://github.com/d3kryption/ubuntu_perforce_server_setup
+```
+
+2) Move the bash file onto your server and run it as sudo.
+
+```bash
+sudo ./perforce_server_setup.sh
+```
+
+3) Some steps will pause execution and wait for you to enter some details to continue.
+
---
+++ perforce_server_setup.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+apt -y update; apt -y upgrade;
+
+# installs
+apt install ufw gpg;
+
+# firewall setup
+ufw default deny incoming;
+ufw default allow outgoing;
+ufw allow ssh;
+ufw allow http;
+ufw allow https;
+
+# perforce
+wget https://package.perforce.com/perforce.pubkey;
+
+gpg -n --import --import-options import-show perforce.pubkey
+
+wget -qO - https://package.perforce.com/perforce.pubkey | sudo apt-key add -;
+
+# Add perforce repo
+cat > /etc/apt/sources.list.d/perforce.list << EOF
+deb http://package.perforce.com/apt/ubuntu focal release
+EOF
+
+# update with new repo
+apt -y update;
+
+# install perforce
+apt-get install helix-p4d;
+
+# perforce settings
+echo "export EDITOR='nano'" >> ~/.bashrc
+echo "export VISUAL='nano'" >> ~/.bashrc
+
+# refresh bashrc
+source ~/.bashrc
+
+# perforce config setup
+sudo /opt/perforce/sbin/configure-helix-p4d.sh
+
+# configure set security settings
+p4 configure set net.rfc3484=1
+p4 configure set dm.user.noautocreate=2
+p4 configure set run.users.authorize=1
+
+# set ignore file
+p4 set P4IGNORE=.p4ignore
+
+# firewall
+echo "press Y to allow firewall"
+
+# allow perforce
+ufw allow 1666;
+
+# enable firewall
+ufw enable -y
+
+# final typemap settings
+p4 typemap;
